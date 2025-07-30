844fd97e0bcfee86eab60ccb74f4f2e20d5a40c8
2910Matheus <matheusmaia2910@gmail.com>
2025-07-30 16:58:53 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import imaplib
+from dotenv import load_dotenv # type: ignore
+import os
+
+load_dotenv()
+
+email = os.getenv("EMAIL")
+senha = os.getenv("SENHA_API")
+servidor_imap = 'imap.gmail.com'
+
+def conectar_email():
+    mail = imaplib.IMAP4_SSL(servidor_imap)
+    status , response = mail.login(email,senha)
+    mail.select("inbox")
+    
+    if status == "OK":
+            print("✅ Conectado e logado com sucesso!")
+    else:
+            print("❌ Erro ao fazer login:", response)
